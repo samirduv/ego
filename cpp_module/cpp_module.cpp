59c3b42573b1c2ef69724bdce6fb78dc382814e5
Samir <samirduv@gmail.com>
2025-08-07 18:23:22 +0200

    feat: implement an approach using canny edge detection

--- cpp_module/cpp_module.cpp
+++ cpp_module/cpp_module.cpp
@@ -47,24 +47,24 @@ py::array_t<uint8_t> blur_largest_shape_in_rect(
     // Crop to region of interest
     cv::Mat roi_img = gray(roi);
 
-    // // Threshold to binary
-    cv::Mat binary;
-    cv::threshold(roi_img, binary, 0, 255, cv::THRESH_BINARY | cv::THRESH_OTSU);
+    // write for debugging
+    cv::imwrite("roi.png", roi_img);
 
-    cv::Mat binary_inverted;
-    cv::threshold(roi_img, binary_inverted, 0, 255, cv::THRESH_BINARY_INV | cv::THRESH_OTSU);
+    cv::Mat img_blur, img_canny;
 
+    // Blurring image using gaussian fliter. Size(3,3) is SE kernal
+    cv::GaussianBlur(roi_img, img_blur, cv::Size(3, 3), 0);
 
+    // edge detection using Canny
+    cv::Canny(img_blur, img_canny, 10, 125);
+
+    // running dilation to close gaps in edges
+    cv::Mat dilated;
+    cv::dilate(img_canny, dilated, cv::Mat(), cv::Point(-1, -1), 2);
 
     // Find contours
     std::vector<std::vector<cv::Point>> contours;
-    cv::findContours(binary, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-
-    std::vector<std::vector<cv::Point>> contours_inverted;
-    cv::findContours(binary_inverted, contours_inverted, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-
-    // Combine contours from both binary and inverted binary images
-    contours.insert(contours.end(), contours_inverted.begin(), contours_inverted.end());
+    cv::findContours(dilated, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
 
     if (contours.empty())
         return input_array; // nothing to blur
